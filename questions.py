8c38c24caf631664a24546782707259ddf2d907f
949027 <82878668+949027@users.noreply.github.com>
2022-05-13 13:28:18 +0700

    Removed folder creation

--- questions.py
+++ questions.py
@@ -3,7 +3,6 @@ import random
 
 
 def get_quiz_set():
-    os.makedirs('quiz-questions', exist_ok=True)
     filename = random.choice(os.listdir('quiz-questions'))
     with open(f'quiz-questions/{filename}', 'r', encoding='KOI8-R') as file:
         tasks = file.read().split(sep='\n\n')
